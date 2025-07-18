dc9fee89e856b18a8ad6b8bc99d8ea7533a1672c
OLASUPOPATRICKOLUSEYE <zachpascal969@gmail.com>
2025-07-18 02:19:53 +0100

    updated empty space

---
+++ src/app/(dashboard)/receptionist/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ReceptionistDashboard = () => {
+  return (
+    <div>
+      ReceptionistDashboard
+    </div>
+  )
+}
+
+export default ReceptionistDashboard
---
+++ src/components/AttendanceChartContainer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AttendanceChartContainer = () => {
+  return (
+    <div>
+      AttendanceChartContainer
+    </div>
+  )
+}
+
+export default AttendanceChartContainer
